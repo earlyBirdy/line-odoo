556cc06c64de3f8f68340fb75f4ccbc7e6e7a42b
Birdy Cho <birdy.cho@gmail.com>
2025-12-31 06:26:19 +0700

    Update odoo

--- odoo/addons/waste_pickup_request/__manifest__.py
+++ odoo/addons/waste_pickup_request/__manifest__.py
@@ -3,7 +3,8 @@
   "version": "0.1.0",
   "depends": ["base", "mail"],
   "data": [
-    "security/ir.model.access.csv",
+    "security/security.xml",
+        "security/ir.model.access.csv",
     "data/sequence.xml",
     "views/pickup_request_views.xml",
     "views/pickup_request_menu.xml"
