e11c5be9642302c55bc6b979ba88cec966972f8a
Birdy Cho <birdy.cho@gmail.com>
2025-12-31 12:54:33 +0700

    Update __manifest__.py

--- odoo/addons/waste_pickup_request/__manifest__.py
+++ odoo/addons/waste_pickup_request/__manifest__.py
@@ -1,14 +1,19 @@
+# -*- coding: utf-8 -*-
 {
-  "name": "Waste Pickup Request (LINE)",
-  "version": "0.1.0",
-  "depends": ["base", "mail"],
-  "data": [
-    "security/security.xml",
+    "name": "Waste Pickup Request (LINE)",
+    "summary": "Waste pickup requests and LINE integration helpers",
+    "version": "0.2.1",
+    "category": "Services",
+    "license": "LGPL-3",
+    "author": "Goodcan / Integration Team",
+    "depends": ["base", "mail"],
+    "data": [
+        "security/security.xml",
         "security/ir.model.access.csv",
-    "data/sequence.xml",
-    "views/pickup_request_views.xml",
+        "views/pickup_request_views.xml",
+        "views/pickup_request_menu.xml",
         "views/pickup_change_request_views.xml",
-    "views/pickup_request_menu.xml"
-  ],
-  "installable": true
+    ],
+    "application": True,
+    "installable": True,
 }
